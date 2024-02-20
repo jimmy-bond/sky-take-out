5498768d82b5a549209478df5128d6454b196b79
jimmy-bond <119022564+jimmy-bond@users.noreply.github.com>
2024-02-20 10:30:01 +0800

    完成阿里云文件上传

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -17,7 +17,7 @@ public interface EmployeeMapper {
      * @param username
      * @return
      */
-    @Select("select * from employee where username = #{username}")
+    @Select("select * from       employee where username = #{username}")
     Employee getByUsername(String username);
     @Insert("insert into employee (name, username, password, phone, sex, id_number, create_time, update_time, create_user, update_user,status) " +
             "values " +
